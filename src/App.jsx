27fa63d1bb7b735c098b75db79de0026705155d0
ManvendraPSdev <welcome2github@gmail.com>
2025-09-28 17:38:34 +0530

    GTA_VI GSAP

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/all" ;
+import Hero from "./sections/Hero.jsx";
+import NavBar from "./sections/NavBar.jsx";
+import FirstVideo from "./sections/FirstVideo.jsx";
+import Jason from "./sections/Jason.jsx";
+import Lucia from "./sections/Lucia.jsx";
+import SecondVideo from "./sections/SecondVideo.jsx";
+import PostCard from "./sections/PostCard.jsx";
+import Final from "./sections/Final.jsx";
+import Outro from "./sections/Outro.jsx";
+
+gsap.registerPlugin(ScrollTrigger) ; 
+
+const App = () => {
+  return(
+    <main>
+      <NavBar/>
+      <Hero/>
+      <FirstVideo/>
+      <Jason/>
+      <SecondVideo/>
+      <Lucia/>
+      <PostCard/>
+      <Final/>
+      <Outro/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav>
+                <img src="/images/nav-logo.svg" className="scale-90"></img>
+                <img src="/images/menu.svg" className="w-10" />
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
